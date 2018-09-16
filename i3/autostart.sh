73d9fa3ce8d36c36000f3fbd536f6dd3b9b6ef0f
Vdimir <vcherckasov@yandex.ru>
2018-09-16 11:23:39 +0300

    Add kblayout to bar, fix wifi indicator

--- i3/autostart.sh
+++ i3/autostart.sh
@@ -1,7 +1,8 @@
 xrandr --output VGA-1 --auto --right-of LVDS-1
 numlockx &
 telegram-desktop -startintray &
-sbxkb &
+xxkb &
+# sbxkb &
 ~/.fehbg
 xinput --set-prop "A4TECH USB Device" "Coordinate Transformation Matrix" 1 0 0 0 1 0 0 0 2
 

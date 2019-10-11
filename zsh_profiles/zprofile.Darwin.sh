271ed7c6aace0193f7b70defa20c01b476b470e9
VDimir <Vdimir@users.noreply.github.com>
2019-10-11 13:35:07 +0300

    Add zprofile.Darwin.sh

---
+++ zsh_profiles/zprofile.Darwin.sh
@@ -0,0 +1,7 @@
+PATH=$PATH:/usr/local/bin
+source /usr/local/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+export LS_COLORS="di=1;34:ln=36:so=35:pi=33:ex=32:bd=34;46:cd=34;43:su=30;41:sg=30;46:tw=30;42:ow=30;43"
+
+alias ls='gls --group-directories-first --color=auto -v'
+
